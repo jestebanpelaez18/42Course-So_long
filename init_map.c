1d40abce9aaa40668c47671974e233058b9b732d
Juan Esteban Pelaez Hoyos <jpelaez-@c2r6p5.hive.fi>
2023-04-03 15:02:21 +0300

    reading the map

--- init_map.c
+++ init_map.c
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:11:55 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/02 18:12:43 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/03 15:01:50 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,24 @@ static int	check_file(int argc, char *argument)
 
 static char	**read_map(t_list *game, char *file)
 {
+	char	*line;
+	char	*full_map;
+
+	line = ft_strdup("");
+	full_map = ft_strdup("");
 	game->fd = open(file, O_RDONLY);
 	if (game->fd == -1)
 		error_msg("Error fd");
-	
-    
+	while (1)
+	{
+		line = get_next_line(game->fd);
+		if (line == NULL)
+			break ;
+		full_map = ft_strjoin(full_map, line);
+	}
+	free(line);
+	full_map = ft_split(full_map, '\n');
+	return (full_map);
 }
 
 void	init_map(t_list *game, int argc, char **argv)
