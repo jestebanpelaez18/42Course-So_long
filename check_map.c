2be86b6a4b4242936529fbbdf19eda5be63831de
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-04-05 18:22:45 +0300

    checked if the map is rectangular

--- check_map.c
+++ check_map.c
@@ -6,15 +6,36 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:53:01 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/05 17:25:57 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/05 18:21:05 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static int	map_rectangular(t_list *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (game->map[i] != NULL)
+	{
+		j = 0;
+		while (game->map[i][j] != '\0')
+			j++;
+		if (j != game->weight)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	check_map(t_list *game)
 {
 	if (game->map == NULL)
 		error_msg("Error, empty map");
 	get_height_weight(game->map, game);
+	if (!map_rectangular(game))
+		error_msg("Error,the map is not rectangular");
 }
