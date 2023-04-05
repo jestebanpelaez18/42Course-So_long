94d7b037cd8220ba04b4baa3707bad928e7f79fc
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-04-05 17:28:48 +0300

    got the height

--- check_map.c
+++ check_map.c
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:53:01 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/05 16:57:41 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:25:57 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	check_map(t_list *game)
 {
 	if (game->map == NULL)
 		error_msg("Error, empty map");
-	get_height_weight(game->map, &game);
+	get_height_weight(game->map, game);
 }
--- check_map_utils.c
+++ check_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 17:27:19 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/05 17:13:46 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:28:30 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	get_height_weight(char **map, t_list *game)
 {
 	int	i;
 	int	w_counter;
+	int	h_counter;
 	int	j;
 
 	i = 0;
@@ -27,6 +28,11 @@ void	get_height_weight(char **map, t_list *game)
 		w_counter++;
 		i++;
 	}
-	// while (map[j][i] != NULL)
-	// 	game->height = j++;
+	h_counter = 1;
+	while (map[j] != NULL)
+	{
+		game->height = h_counter;
+		h_counter++;
+		j++;
+	}
 }
