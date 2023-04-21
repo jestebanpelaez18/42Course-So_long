059e73e62a4d27fda8921723f960b6c77630540b
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-04-21 18:12:42 +0300

    key code created

--- start_game.c
+++ start_game.c
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 14:34:30 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/21 17:42:35 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/21 18:12:24 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,26 @@ static void	put_image(t_list *game)
 		{
 			if (game->map[i][j] == '1')
 				mlx_put_image_to_window(game->mlx, game->win, game->water,
-					x_axis, y_axis);
+						x_axis, y_axis);
 			if (game->map[i][j] == '0')
 				mlx_put_image_to_window(game->mlx, game->win, game->grass,
-					x_axis, y_axis);
+						x_axis, y_axis);
 			if (game->map[i][j] == 'E')
 				mlx_put_image_to_window(game->mlx, game->win, game->exit,
-					x_axis, y_axis);
+						x_axis, y_axis);
 			if (game->map[i][j] == 'C')
 			{
 				mlx_put_image_to_window(game->mlx, game->win, game->grass,
-					x_axis, y_axis);
+						x_axis, y_axis);
 				mlx_put_image_to_window(game->mlx, game->win, game->collect,
-					x_axis, y_axis);
+						x_axis, y_axis);
 			}
 			if (game->map[i][j] == 'P')
 			{
 				mlx_put_image_to_window(game->mlx, game->win, game->grass,
-					x_axis, y_axis);
+						x_axis, y_axis);
 				mlx_put_image_to_window(game->mlx, game->win, game->player,
-					x_axis, y_axis);
+						x_axis, y_axis);
 			}
 			y_axis += 120;
 			j++;
@@ -71,13 +71,29 @@ static void	set_image(t_list *game)
 			&img_height);
 	game->grass = mlx_xpm_file_to_image(game->mlx, "img/freegrass.xpm",
 			&img_width, &img_height);
-	game->exit = mlx_xpm_file_to_image(game->mlx, "img/hueco.xpm",
-			&img_width, &img_height);
+	game->exit = mlx_xpm_file_to_image(game->mlx, "img/hueco.xpm", &img_width,
+			&img_height);
 }
-static int keys(int key_code, t_list *game)
+
+static int	keys(int key_code, t_list *game)
 {
-	
+	if (key_code == 2 || key_code = 124)
+		right(game);
+	else if (key_code == 0 || key_code = 123)
+		left(game);
+	else if (key_code == 1 || key_code = 125)
+		down(game);
+	else if (key_code == 1 || key_code = 126)
+		up(game);
+	else if (key_code == 53)
+	{
+		free_argt(game->map);
+		mlx_destroy_window(game->mlx, game->win);
+		exit(1);
+	}
+	return(0);
 }
+
 void	start_game(t_list *game)
 {
 	game->mlx = mlx_init();
@@ -85,6 +101,6 @@ void	start_game(t_list *game)
 			* 120, "So Long Juanes");
 	set_image(game);
 	put_image(game);
-	mlx_hook(game->win, 2 , 1L<<0, keys, game);
+	mlx_hook(game->win, 2, 1L << 0, keys, game);
 	mlx_loop(game->mlx);
 }
