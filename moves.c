0a7ff17634fac80cf876738274b9c2b91e1766db
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-04-26 17:45:05 +0300

    comments in the terminal

--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 18:45:02 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/26 17:38:09 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/26 17:44:08 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void	right(t_list *game, int y, int x)
 		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
 		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
 			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
+		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
 	}
 }
 
@@ -63,6 +64,7 @@ void	left(t_list *game, int y, int x)
 		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
 		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
 			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
+		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
 	}
 }
 
@@ -90,6 +92,7 @@ void	up(t_list *game, int y, int x)
 		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
 		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
 			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
+		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
 	}
 }
 
@@ -117,5 +120,6 @@ void	down(t_list *game, int y, int x)
 		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
 		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
 			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
+		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
 	}
 }
