8c58242ea0b7c036fb759689a27deace58936e24
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-04-26 16:40:49 +0300

    fixed problem with the exit

--- moves.c
+++ moves.c
@@ -6,18 +6,11 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 18:45:02 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/26 16:00:37 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/26 16:39:55 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-static void	check_collect(t_list *game, int y, int x)
-{
-	game->n_collect--;
-	if (game->n_collect == 0)
-		exit_game(game);
-}
 
 void	right(t_list *game, int y, int x)
 {
@@ -27,13 +20,14 @@ void	right(t_list *game, int y, int x)
 	{
 		if (game->n_collect == 0)
 			exit_game(game);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, x, y);
 	}
 	else if (game->map[y / 64][(x / 64) + 1] != '1')
 	{
 		game->p_x += 64;
 		if (game->map[y / 64][(x / 64) + 1] == 'C')
 		{
-			check_collect(game, y, game->p_x);
+			game->n_collect--;
 			game->map[y / 64][(x / 64) + 1] = '0';
 		}
 		mlx_put_image_to_window(game->mlx, game->win, game->player, x + 64, y);
@@ -48,13 +42,14 @@ void	left(t_list *game, int y, int x)
 	{
 		if (game->n_collect == 0)
 			exit_game(game);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, x, y);
 	}
 	else if (game->map[y / 64][(x / 64) - 1] != '1')
 	{
 		game->p_x -= 64;
 		if (game->map[y / 64][(x / 64) - 1] == 'C')
 		{
-			check_collect(game, y, game->p_x);
+			game->n_collect--;
 			game->map[y / 64][(x / 64) - 1] = '0';
 		}
 		mlx_put_image_to_window(game->mlx, game->win, game->player, x - 64, y);
@@ -69,13 +64,14 @@ void	up(t_list *game, int y, int x)
 	{
 		if (game->n_collect == 0)
 			exit_game(game);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, x, y);
 	}
 	else if (game->map[(y / 64) - 1][x / 64] != '1')
 	{
 		game->p_y -= 64;
 		if (game->map[(y / 64) - 1][x / 64] == 'C')
 		{
-			check_collect(game, game->p_y, x);
+			game->n_collect--;
 			game->map[(y / 64) - 1][x / 64] = '0';
 		}
 		mlx_put_image_to_window(game->mlx, game->win, game->player, x, y - 64);
@@ -90,13 +86,14 @@ void	down(t_list *game, int y, int x)
 	{
 		if (game->n_collect == 0)
 			exit_game(game);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, x, y);
 	}
 	else if (game->map[(y / 64) + 1][x / 64] != '1')
 	{
 		game->p_y += 64;
 		if (game->map[(y / 64) + 1][x / 64] == 'C')
 		{
-			check_collect(game, game->p_y, x);
+			game->n_collect--;
 			game->map[(y / 64) + 1][x / 64] = '0';
 		}
 		mlx_put_image_to_window(game->mlx, game->win, game->player, x, y + 64);
