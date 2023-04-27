516613faa614aa69cea5c838c5691d3f2a1dee8d
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-04-27 15:11:20 +0300

    fixed leaks with the text

--- moves.c
+++ moves.c
@@ -6,16 +6,29 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 18:45:02 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/04/27 13:34:56 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/04/27 15:10:14 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	put_text(t_list *game)
+{
+	char	*n_moves;
+	char	*string;
+
+	n_moves = ft_itoa(game->moves);
+	string = ft_strjoin("Nºmoves: ", n_moves);
+	mlx_put_image_to_window(game->mlx, game->win, game->water, 0, 0);
+	mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
+	mlx_string_put(game->mlx, game->win, 0, 0, 0x008000, string);
+	ft_putendl_fd(string, 1);
+	free(string);
+	free(n_moves);
+}
+
 void	right(t_list *game, int y, int x)
 {
-	// if (game->map[(y / 64)][(x / 64) + 1] != '1')
-	// 	mlx_put_image_to_window(game->mlx, game->win, game->grass, x, y);
 	if (game->map[y / 64][(x / 64) + 1] == 'E')
 	{
 		if (game->n_collect == 0)
@@ -32,19 +45,14 @@ void	right(t_list *game, int y, int x)
 			game->map[y / 64][(x / 64) + 1] = '0';
 		}
 		game->moves++;
-		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x, game->p_y);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 0, 0);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
-		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
-			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
-		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x,
+			game->p_y);
+		put_text(game);
 	}
 }
 
 void	left(t_list *game, int y, int x)
 {
-	// if (game->map[(y / 64)][(x / 64) - 1] != '1')
-	// 	mlx_put_image_to_window(game->mlx, game->win, game->grass, x, y);
 	if (game->map[y / 64][(x / 64) - 1] == 'E')
 	{
 		if (game->n_collect == 0)
@@ -61,19 +69,14 @@ void	left(t_list *game, int y, int x)
 			game->map[y / 64][(x / 64) - 1] = '0';
 		}
 		game->moves++;
-		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x, game->p_y);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 0, 0);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
-		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
-			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
-		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x,
+			game->p_y);
+		put_text(game);
 	}
 }
 
 void	up(t_list *game, int y, int x)
 {
-	// if (game->map[(y / 64) - 1][x / 64] != '1')
-	// 	mlx_put_image_to_window(game->mlx, game->win, game->grass, x, y);
 	if (game->map[(y / 64) - 1][x / 64] == 'E')
 	{
 		if (game->n_collect == 0)
@@ -90,19 +93,14 @@ void	up(t_list *game, int y, int x)
 			game->map[(y / 64) - 1][x / 64] = '0';
 		}
 		game->moves++;
-		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x, game->p_y);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 0, 0);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
-		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
-			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
-		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x,
+			game->p_y);
+		put_text(game);
 	}
 }
 
 void	down(t_list *game, int y, int x)
 {
-	// if (game->map[(y / 64) + 1][x / 64] != '1')
-	// 	mlx_put_image_to_window(game->mlx, game->win, game->grass, x, y);
 	if (game->map[(y / 64) + 1][x / 64] == 'E')
 	{
 		if (game->n_collect == 0)
@@ -119,11 +117,8 @@ void	down(t_list *game, int y, int x)
 			game->map[(y / 64) + 1][x / 64] = '0';
 		}
 		game->moves++;
-		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x, game->p_y);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 0, 0);
-		mlx_put_image_to_window(game->mlx, game->win, game->water, 64, 0);
-		mlx_string_put(game->mlx, game->win, 0, 0, 0x008000,
-			ft_strjoin("Nºmoves: ", ft_itoa(game->moves)));
-		ft_putendl_fd(ft_strjoin("Nºmoves: ", ft_itoa(game->moves)), 1);
+		mlx_put_image_to_window(game->mlx, game->win, game->player, game->p_x,
+			game->p_y);
+		put_text(game);
 	}
 }
